65bd550df03fd22afd6c180a50b79950a37b9ded
Mishu78 <meherun2002@gmail.com>
2026-03-02 15:03:33 +0600

    designing div

--- posts.js
+++ posts.js
@@ -4,28 +4,43 @@ const loadPost = () =>{
     .then((json)=>displayPost(json))
 };
 //receiving array of object
- const displayPost=(posts)=>{
-    //1.get the container
-  const postContainer=document.getElementById('post-container');
-  postContainer.innerText='';
-  //console.log(postContainer);
+//  const displayPost=(posts)=>{
+//     //1.get the container
+//   const postContainer=document.getElementById('post-container');
+//   postContainer.innerText='';
+//   //console.log(postContainer);
 
-    posts.forEach(post=>{
-        console.log(post.title);
-        //create HTML element
-        const li=document.createElement('li');
-        li.innerText=post.title;
-        console.log(li);
+//     posts.forEach(post=>{
+//         console.log(post.title);
+//         //create HTML element
+//         const li=document.createElement('li');
+//         li.innerText=post.title;
+//         console.log(li);
 
-        //3.add li into container
+//         //3.add li into container
 
-        postContainer.appendChild(li);
-    })
-    // for(let post of posts){
-    //     console.log(post);
-    // }
-    // for(let i =0;i<=posts.length;i++){
-    //     console.log(posts[i]);
-    // }
-    }
+//         postContainer.appendChild(li);
+//     })
+//     // for(let post of posts){
+//     //     console.log(post);
+//     // }
+//     // for(let i =0;i<=posts.length;i++){
+//     //     console.log(posts[i]);
+//     // }
+//  }
+
+
+// {
+//     "userId": 5,
+//     "id": 50,
+//     "title": "repellendus qui recusandae incidunt voluptates tenetur qui omnis exercitationem",
+//     "body": "error suscipit maxime adipisci consequuntur recusandae\nvoluptas eligendi et est et voluptates\nquia distinctio ab amet quaerat molestiae et vitae\nadipisci impedit sequi nesciunt quis consectetur"
+// }
+const displayPost =(posts)=>{
+    const postContainer = document.getElementById('post-container');
+    postContainer.innerText='';
+ posts.forEach(post=>{
+    
+ })
+}
     
