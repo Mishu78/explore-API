66cc992be427e983d8514f525254f27d4b1dd1c4
Mishu78 <meherun2002@gmail.com>
2026-03-02 13:32:14 +0600

    loaded data and each data displayed in console

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const loadData=()=>{
 fetch('https://jsonplaceholder.typicode.com/todos/1')
+//promise of response
       .then(response => response.json())
       .then(data => console.log(data))
-} 
+};
+
+const loadPost=()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res)=>res.json())
+    .then((json)=>displayPost(json))
+};
+const displayPost =(posts)=>{
+    posts.forEach(post=>{
+        console.log(post);
+    })
+}
