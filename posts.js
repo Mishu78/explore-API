b1ec5847f805d4f451f508856e01309040426ce3
Mishu78 <meherun2002@gmail.com>
2026-03-02 14:32:05 +0600

    li created in a container

--- posts.js
+++ posts.js
@@ -3,16 +3,28 @@ const loadPost = () =>{
     .then((res)=>res.json())
     .then((json)=>displayPost(json))
 };
-
+//receiving array of object
  const displayPost=(posts)=>{
-    // posts.forEach(post=>{
-    //     console.log(post);
-    // })
+    //1.get the container
+  const postContainer=document.getElementById('post-container');
+  //console.log(postContainer);
+
+    posts.forEach(post=>{
+        console.log(post.title);
+        //create HTML element
+        const li=document.createElement('li');
+        li.innerText=post.title;
+        console.log(li);
+
+        //3.add li into container
+
+        postContainer.appendChild(li);
+    })
     // for(let post of posts){
     //     console.log(post);
     // }
-    for(let i =0;i<=posts.length;i++){
-        console.log(posts[i]);
-    }
+    // for(let i =0;i<=posts.length;i++){
+    //     console.log(posts[i]);
+    // }
     }
     
