11959d9d498c1694d71e210a62cbb907f0f5dd4b
Mishu78 <meherun2002@gmail.com>
2026-03-02 12:03:15 +0600

    loaded data when button is clicked

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+const loadData=()=>{
 fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
-      .then(json => console.log(json))
-
-      
+      .then(data => console.log(data))
+} 
